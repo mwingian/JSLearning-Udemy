d8c2f6384cc7e677241e6bbfc8da6d84317c7669
Serhii <morowingerthemain@gmail.com>
2023-04-05 12:49:16 +0300

    Movie DB object added

--- js/my-js.js
+++ js/my-js.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let questionOne = prompt("Один из последних просмотренных фильмов?", ""),
+  questionTwo = prompt("На сколько оцените его?", ""),
+  questionThree = prompt("Один из последних просмотренных фильмов?", ""),
+  questionFour = prompt("На сколько оцените его?", "");
+
+let movieName = questionOne,
+  secondMovieName = questionThree;
+
+personalMovieDB.movies[movieName] = questionTwo;
+personalMovieDB.movies[secondMovieName] = questionFour;
+
+// personalMovieDB.movies = {
+//   questionOne: questionTwo,
+//   questionThree: questionFour,
+// };
